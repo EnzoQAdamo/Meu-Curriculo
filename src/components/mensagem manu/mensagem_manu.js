213ca1bc725514142fe945398f08b8b120def4e1
Enzo Quartieri Adamo <75939399+EnzoQAdamo@users.noreply.github.com>
2024-11-13 16:23:06 -0300

    mensagem de aniver

--- src/components/mensagem manu/mensagem_manu.js
+++ src/components/mensagem manu/mensagem_manu.js
@@ -4,7 +4,8 @@ const mensagemManu = () => {
   return(
     <div className='conteudo'>
         <div className='conteudo'>
-            <h1>Te amo meu xuxuzin</h1>
+            <h1>Feliz aniversário meu xuxuzin lindo</h1>
+            <p>Feliz aniversário, meu amor! Mesmo com a distância entre nós, meu coração segue com você em cada passo, torcendo, vibrando e amando ainda mais. Hoje, tudo o que eu queria era estar ao seu lado, mas como sou vagabundo te fiz chorar no seu dia. Queria estar ai e  celebrar cada sorriso seu e te envolver em um abraço que dissesse o quanto você é especial para mim. Cada dia ao seu lado, mesmo que à distância, só reforça a certeza do quanto te amo e do quanto você é a pessoa que quero ter ao meu lado. Parabéns, minha pessoa favorita. Te amo muito, não esquece!</p>
         </div>
         <div class="container">
             <div class="preloader">
